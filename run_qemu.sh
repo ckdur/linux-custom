43f11dc9578614d4053ca9df3a73cadaded2971b
Ckristian Duran <ckdur.iso@gmail.com>
2021-02-02 15:10:08 +0900

    Initial commit

---
+++ run_qemu.sh
@@ -0,0 +1,1 @@
+qemu-system-riscv64 -M virt -nographic -m 256M -bios opensbi/build/platform/generic/firmware/fw_payload.elf
---
+++ source.sh
@@ -0,0 +1,5 @@
+export ARCH=riscv
+export CROSS_COMPILE=riscv64-unknown-linux-gnu-
+export RISCV=/opt/riscv64gc
+. riscv.sh
+
