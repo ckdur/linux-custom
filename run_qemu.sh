cb17a44e686e92c91d1a25e3f05c03765524fbff
Ckristian Duran <ckdur.iso@gmail.com>
2021-07-25 17:58:32 +0900

    Ratona board qemu stuff

--- run_qemu.sh
+++ run_qemu.sh
@@ -4,5 +4,13 @@ if [ ! -f "$LINUX_IMAGE" ]; then
   echo "$LINUX_IMAGE does not exists. Please run_opensbi.sh."
   exit 1
 fi
-qemu-system-riscv64 -M virt -nographic -m 256M -bios $BIOS_FILE
 
+cd qemu 
+./configure --target-list=riscv32-softmmu
+cd ..
+make -C qemu -j $(nproc)
+sudo -C qemu make install
+
+#qemu-system-riscv64 -M virt -nographic -m 256M -bios $BIOS_FILE
+qemu-system-riscv32 -M ratona,dumpdtb=./test_dts/qemu.dtb  -m 32M -nographic -bios ./opensbi/build/platform/ratona/firmware/fw_payload.elf -s -S
+dtc -I dtb -O dts test_dts/qemu.dtb > test_dts/qemu.dts
